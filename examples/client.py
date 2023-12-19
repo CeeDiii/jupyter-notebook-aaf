fef79e533c8dcd8c5847b5d68fb28c360bb28493
Cedric Altermatt <cedric.altermatt@gmail.com>
2023-12-19 11:06:22 +0100

    [refactor]: make client configurable

--- examples/client.py
+++ examples/client.py
@@ -3,9 +3,26 @@ import json
 import os
 import sys
 import time
+from typing import TypedDict
 
 from dotenv import load_dotenv
 import requests
+
+
+class ClientConfig(TypedDict):
+    DEV_BASE_URL: str
+    PROD_BASE_URL: str
+    ORCHESTRATOR_PATH: str
+    NOTEBOOK_PATH: str
+
+
+def read_json_config(file_path: str) -> ClientConfig:
+    """
+    read the config file and return it as dict
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        config = json.load(f)
+        return config
 
 
 def get_status(status_uri: str):
@@ -33,20 +50,41 @@ def main():
         description="A client to test the Jupyter-notebook-as-a-function locally or in production",
     )
     parser.add_argument(
-        "-e", "--env", required=True, choices=["dev", "prod"], default="dev"
+        "-e",
+        "--env",
+        required=True,
+        choices=["dev", "prod"],
+        default="dev",
+        help="Define if you want to query the local endpoint in development or the deployed endpoint in production.",
     )
+
+    parser.add_argument(
+        "-n",
+        "--notebook",
+        required=False,
+        help="The path to the notebook that you want to execute. Remember that the notebooks have to be stored in the 'jupyter-notebooks' blob container.",
+    )
+
     args = vars(parser.parse_args(sys.argv[1:]))
     print(f"Querying for execution in environment {args['env']}")
 
     load_dotenv()
+    script_path = os.path.dirname(sys.argv[0])
+    config_path = script_path + "/client.config.json"
+    client_config = read_json_config(config_path)
 
-    base_url = "http://localhost:7071"
-    API_PATH = "/api/orchestrators/execute_notebook?notebook_path=example/example.ipynb"
+    base_url = client_config["DEV_BASE_URL"]
+    orchestrator_path = client_config["ORCHESTRATOR_PATH"]
+    notebook_path = client_config["NOTEBOOK_PATH"]
 
     if args["env"] == "prod":
-        base_url = "https://jupyter-notebook-as-a-function.azurewebsites.net"
+        base_url = client_config["PROD_BASE_URL"]
 
-    request_url = base_url + API_PATH
+    user_notebook_path = args.get("notebook")
+    if user_notebook_path is not None:
+        notebook_path = user_notebook_path
+
+    request_url = base_url + orchestrator_path + notebook_path
 
     res = requests.post(
         request_url,
